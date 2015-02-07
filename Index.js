cc391c16bb8815979f438949d1aea6a62ba70f18
unknown <helsingborgsucks@gmail.com>
2015-02-07 02:02:01 +0100

    Routing etc.

---
+++ Index.js
@@ -0,0 +1,103 @@
+﻿angular.module('soccerApp', ['angular.filter', 'ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'pages/home.html',
+                controller: 'HomeController'
+            })
+            .when('/debug', {
+                templateUrl: 'pages/debug.html',
+                controller: 'DebugController'
+            })
+            .when('/gamesperteam/:year?/:team?', {
+                templateUrl: 'pages/gamesperteam.html',
+                controller: 'GamesperteamController'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
+    .controller('NavigationController', [
+        '$scope', '$route',
+        function ($scope, $route) {
+            $scope.$route = $route;
+            $scope.routes = [
+                {
+                    name: 'Home',
+                    controllerName: 'HomeController',
+                    route: '#/'
+                },
+                {
+                    name: 'Games per team',
+                    controllerName: 'GamesperteamController',
+                    route: '#/gamesperteam'
+                },
+                {
+                    name: 'Debug',
+                    controllerName: 'DebugController',
+                    route: '#/debug'
+                }
+            ];
+        }
+    ])
+    .controller('GamesperteamController', [
+        '$scope', 'eventService', '$routeParams',
+        function ($scope, eventService, $routeParams) {
+            $scope.params = $routeParams;
+            $scope.years = [];
+            for (var i = 92; i < 115; i++) {
+                $scope.years.push((1900 + i) + "-" + (1901 + i));
+            }
+            if (!$scope.params.year) $scope.params.year = $scope.years[0];
+
+            $scope.games = [];
+
+            $scope.teams = [];
+            function setTeams(result) {
+                $scope.teams = result.data;
+                if (!$scope.params.team) $scope.params.team = Object.keys($scope.teams)[0];
+            }
+            eventService.GetAllTeams($scope.params.year).then(setTeams);
+
+            function setGames(result) {
+                $scope.games = result.data;
+            }
+            $scope.changeYear = function () {
+                eventService.GetGamesPerTeam($scope.params.year, $scope.params.team).then(setGames);
+            }
+            $scope.changeYear($scope.params.year);
+        }
+    ])
+    .controller('HomeController', [
+        function () {
+        }
+    ])
+    .controller('DebugController', [
+        function () {
+        }
+    ])
+    .service('eventService', [
+        '$http', '$filter',
+        function ($http, $filter) {
+            function getGamesPerTeam(year, team) {
+                function filterByTeam(promise) {
+                    promise.data = $filter('filter')(promise.data, { $: team });
+                    return promise;
+                }
+                return $http.get('node/output/' + year + '.json').then(filterByTeam);
+            }
+
+            function getAllTeams(year) {
+                function filterHomeTeams(promise) {
+                    promise.data = $filter('groupBy')(promise.data, 'HomeTeam');
+                    return promise;
+                }
+                return $http.get('node/output/' + year + '.json').then(filterHomeTeams);
+            }
+
+            return {
+                GetGamesPerTeam: getGamesPerTeam,
+                GetAllTeams: getAllTeams
+            }
+        }
+    ]);
---
+++ node/parser.js
@@ -0,0 +1,57 @@
+﻿var jsdom = require("jsdom");
+var fs = require('fs');
+
+function doIt(name) {
+    var fileName = "output/" + name + ".json";
+    var url = "http://www.premierleague.com/en-gb/matchday/results.html?paramClubId=ALL&paramComp_8=true&paramSeason=" + name + "&view=.dateSeason";
+
+    console.log("Beginning " + fileName);
+    jsdom.env({
+        url: url,
+        scripts: ["http://code.jquery.com/jquery.js"],
+        done: function (errors, window) {
+            if (!window || !window.$) {
+                console.log("No jQuery, " + fileName + " failed.");
+                return;
+            }
+
+            console.log("Parsing " + fileName);
+
+            var $ = window.$;
+
+            var events = [];
+            var contentTables = $('.contentTable');
+            contentTables.each(function (index) {
+                var $this = $(this);
+                var date = $this.find('.masthead').parent().text();
+
+                var games = $this.find('.fixturechangerow');
+                if (games.length == 0) {
+                    games = $this.find('tr.alt');
+                }
+
+                games.each(function () {
+                    var $this = $(this);
+                    var event = {};
+                    event.Date = date;
+                    event.HomeTeam = $this.find('.rHome').find('a').text();
+                    event.AwayTeam = $this.find('.rAway').find('a').text();
+                    var score = $this.find('.score').find('a').text().split(' - ');
+                    event.HomeScore = score[0];
+                    event.AwayScore = score[1];
+                    event.Location = $this.find('.location').find('a').text();
+                    events.push(event);
+                });
+            });
+
+            console.log("Begin write " + fileName);
+            fs.writeFileSync(fileName, JSON.stringify(events));
+            console.log("End write " + fileName);
+        }
+    });
+}
+
+for (var i = 106; i < 115; i++) {
+    var name = (1900 + i) + "-" + (1901 + i);
+    doIt(name);
+}
